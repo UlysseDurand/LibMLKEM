c8df39777c7c9b5622fcfdd1c90222df3db0350c
Ulysse Durand <uldr@pm.me>
2024-05-16 18:14:09 +0200

    belle avance

--- spark_ada/src/mlkem.ads
+++ spark_ada/src/mlkem.ads
@@ -2,11 +2,93 @@
 --  SPDX-License-Identifier: Apache-2.0
 
 with SPARK.Big_Integers; use SPARK.Big_Integers;
+with SPARK.Big_Intervals; use SPARK.Big_Intervals;
 
 with Interfaces; use Interfaces;
 package MLKEM
   with SPARK_Mode => On
 is
+
+   --  Our lemmas for the SPARK proof
+
+   -- Prove a number is prime
+
+   --  function givesBigInterval (A : Big_Integer ; B : Big_Integer) return Interval with
+   --     Post => (for all X in givesBigIntervalBasic (A, B) => In_Range(givesBigInterval(A, B), X)); 
+
+   --  function Is_Prime (N : Big_Natural) return Boolean is
+   --  (for all J in givesBigInterval(2,N) => ( N mod J /= 0));
+
+   --  procedure Number_Is_Prime (N : Big_Natural)
+   --  with
+   --     Ghost,
+   --     Global => null,
+   --     Post => Is_Prime(N);
+
+   function obviousComp(A : Big_Integer; B : Big_Integer) return Boolean with
+      Ghost,
+      Pre => B /= 0,
+      Post => A/B <= abs A;
+
+
+   -- Correspondance between definitions of one number divides another
+   function CorrespDividOne(A : Big_Integer; B : Big_Integer) return Big_Integer with
+      Ghost,
+      Pre => B /= 0 and then (A mod B = 0),
+      Post =>  
+         In_Range(Interval'(-abs A, abs A), CorrespDividOne'Result) and 
+         (A = CorrespDividOne'Result * B);
+
+   function CorrespDividTwo(A : Big_Integer; B : Big_Integer) return Boolean with
+      Ghost,
+      Pre => B /= 0 and then (for some k in Interval'(-abs A, abs A) => (A = k * B)),
+      Post => CorrespDividTwo'Result and A mod B = 0;
+  
+   --  if B divides A then B divides A * M
+   function goodDivideMult(A : Big_Integer; B : Big_Integer; M : Big_Integer) return Boolean with
+      Ghost,
+      Pre => B /= 0 and then(A mod B = 0),
+      Post => A * M mod B = 0;
+
+   function goodDivideAdd(B: Big_Integer; C: Big_Integer; D: Big_Integer) return Boolean with
+      Ghost,
+      Pre => B /= 0 and then(C mod B = 0 and D mod B =0),
+      Post => (C + D) mod B = 0;
+
+   type Ext_cd is record 
+      U : Big_Integer;
+      V : Big_Integer;
+      D : Big_Natural;
+   end Record;
+
+   function ext_gcd (A : Big_Natural; B : Big_Natural) return Ext_cd
+   with
+      Ghost,
+      Annotate => (GNATprove, Always_Return),
+      Subprogram_Variant => (Decreases => B),
+      Pre => B /= 0,
+      Post =>
+         ext_gcd'Result.D > 0 and
+         A * ext_gcd'Result.U + B * ext_gcD'Result.V = ext_gcd'Result.D and
+         A mod ext_gcd'Result.D = 0 and
+         B mod ext_gcd'Result.D = 0;
+
+
+
+   --  function Lemma_prime_divides_product
+   --     (A : Big_Natural ;
+   --     B : Big_Natural ;
+   --     P : Big_Natural )
+   --  return Boolean with
+   --     Ghost,
+   --     Global=>null,
+   --     Pre=>
+   --        Is_Prime(P) and
+   --        (A * B) mod P = 0,
+   --     Post =>
+   --        A mod P = 0 or 
+   --        B mod P = 0
+   --        ;
    --==============================================
    --  Exported constants, types, and subprograms
    --
@@ -191,54 +273,5 @@ private
       pragma Unreferenced ("mod");
       pragma Unreferenced ("rem");
    end Zq;
-
-   --  Our lemmas for the SPARK proof
-
-   -- Prove a number is prime
-
-   function Is_Prime (N : Big_Integer) return Boolean is
-   (for all J in Big_Integer => (if 1<J and J<N then ( N mod J /= 0)));
-
-   procedure Number_Is_Prime (N : Big_Integer)
-   with
-      Ghost,
-      Global => null,
-      Post => Is_Prime(N);
-   
-
-
-   type Ext_cd is array (1 .. 3) of Big_Integer;
-
-   function Lemma_extended_common_divisor
-      (A : Big_Integer ;
-       B : Big_Integer ) return Ext_cd
-   with
-      Ghost,
-      Global => null,
-      Post => (
-         for some U in Big_Integer => (
-            for some V in Big_Integer => (
-               for some D in Big_Integer => (
-                  A * U + B * V = D and
-                  A mod D = 0 and
-                  B mod D = 0
-               )
-            )
-         )
-      ) ;
-
-   function Lemma_prime_divides_product
-      (A : Big_Integer ;
-      B : Big_Integer ;
-      P : Big_Integer )
-   return Boolean with
-      Ghost,
-      Global=>null,
-      Pre=>
-         Is_Prime(P) and
-         (A * B) mod P = 0,
-      Post =>
-         A mod P = 0 or 
-         B mod P = 0;
 
 end MLKEM;
